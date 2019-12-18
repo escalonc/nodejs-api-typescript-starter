73ef86574be42bca9cec559a9554cdad2b0e606b
Rafael Sequeiros <rsequeiros@supports-MacBook-Pro.local>
2019-12-18 15:53:20 -0600

    git added sqllite in memory

--- src/modules/todos/__tests__/todoService.spec.ts
+++ src/modules/todos/__tests__/todoService.spec.ts
@@ -1,24 +1,28 @@
-import TodoRepository from "../todoRepository";
-import TodoService from "../todoService";
+import {createConnection, getConnection, getRepository} from "typeorm";
+import TodoEntity from "../todoEntity";
+
+beforeEach(() => {
+  return createConnection({
+    type: "sqlite",
+    database: ":memory",
+    dropSchema: true,
+    entities: [TodoEntity],
+    synchronize: true,
+    logging: false,
+  });
+});
+
+afterEach(() => {
+  let conn = getConnection();
+  return conn.close();
+});
 
 jest.mock("../todoRepository");
 
-const mockRepository: jest.Mocked<TodoRepository> = new TodoRepository() as any;
-mockRepository.find.mockImplementation(
-  () =>
-    new Promise(resolve =>
-      resolve([
-        {id: 1, name: "Go to meet"},
-        {id: 2, name: "Example"},
-      ]),
-    ),
-);
-
 test("getAll returns a collection of todos", async () => {
-  const todoService = new TodoService(mockRepository);
-  const data = await todoService.getAllTodos();
-  expect(data).toStrictEqual([
-    {id: 1, name: "Go to meet"},
-    {id: 2, name: "Example"},
-  ]);
+  await getRepository(TodoEntity).insert({
+    name: "Rafael Sequeiros",
+  });
+  const data = await getRepository(TodoEntity).find();
+  expect(data).toEqual([{id: 1, name: "Rafael Sequeiros"}]);
 });
