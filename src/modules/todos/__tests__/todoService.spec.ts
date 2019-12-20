2e9545872eed7c6dbaf226ed836646f9d8317924
Christopher Escalon <escalonchristopher@gmail.com>
2019-12-19 22:27:35 -0600

    setting up sqlite with connection

--- src/modules/todos/__tests__/todoService.spec.ts
+++ src/modules/todos/__tests__/todoService.spec.ts
@@ -22,4 +22,30 @@ test('getAll returns a collection of todos', async () => {
     { id: 1, name: 'Go to meet' },
     { id: 2, name: 'Example' }
   ]);
+
+
+beforeEach(() => {
+  return createConnection({
+    type: "sqlite",
+    database: ":memory",
+    dropSchema: true,
+    entities: [TodoEntity],
+    synchronize: true,
+    logging: false,
+  });
+});
+
+afterEach(() => {
+  let conn = getConnection();
+  return conn.close();
+});
+
+jest.mock("../todoRepository");
+
+test("getAll returns a collection of todos", async () => {
+  await getRepository(TodoEntity).insert({
+    name: "Rafael Sequeiros",
+  });
+  const data = await getRepository(TodoEntity).find();
+  expect(data).toEqual([{id: 1, name: "Rafael Sequeiros"}]);
 });
