543c8dcb7cc8e18f9447cd7e994ff1503dc8fcbf
Christopher Escalon <escalonchristopher@gmail.com>
2019-12-20 10:41:44 -0600

    test

--- src/modules/todos/__tests__/todoService.spec.ts
+++ src/modules/todos/__tests__/todoService.spec.ts
@@ -1,22 +1,9 @@
 import TodoRepository from '../todoRepository';
 import TodoService from '../todoService';
 import ConnectionFactory from '../../../data/factories/defaultConnectionFactory';
-import { getCustomRepository, Connection } from 'typeorm';
+import { Connection } from 'typeorm';
 
-// jest.mock('../todoRepository');
 let connectionFactory = new ConnectionFactory();
-
-// const mockRepository: jest.Mocked<TodoRepository> = new TodoRepository() as any;
-// mockRepository.find.mockImplementation(
-//   () =>
-//     new Promise(resolve =>
-//       resolve([
-//         { id: 1, name: 'Go to meet' },
-//         { id: 2, name: 'Example' }
-//       ])
-//     )
-// );
-
 let connection: Connection;
 
 beforeEach(async () => {
@@ -28,10 +15,10 @@ afterEach(async () => {
 });
 
 test('getAll returns a collection of todos', async () => {
-  const todoRepository = getCustomRepository(TodoRepository);
-  todoRepository.create({ id: 1, name: 'Go to meet' });
+  const todoRepository = connection.getCustomRepository(TodoRepository);
+  todoRepository.insert({ id: 1, name: 'Go to meet' });
   const todoService = new TodoService(todoRepository);
   const data = await todoService.getAllTodos();
 
-  expect(data).toStrictEqual({ id: 1, name: 'Go to meet' });
+  expect(data).toEqual([{ id: 1, name: 'Go to meet' }]);
 });
